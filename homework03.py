b7d344d30ddbf373d8a74f0ae458eed0373f69c4
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-05-16 09:03:20 +0300

    Update homework03.py

--- homework03.py
+++ homework03.py
@@ -1,8 +1,8 @@
 name = 'Nadin'
 print('Name ' + name)
 age = 44
-print('Age ' + str(age))
-age = str(age + 1)
+print('Age ', (age))
+age = (age + 1)
 print('New Age', age)
-is_student = str(True)
-print('Is Student ' + is_student)
+is_student = True
+print('Is Student ', is_student)
